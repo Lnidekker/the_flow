455f68b3c1b5760f872de54561a1b59818fb8f52
Lnidekker <leonidnidekker@gmail.com>
2022-08-22 23:33:02 +0400

    corr README

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='the_flow',
     packages=['the_flow', 'the_flow.templates'],
     include_package_data=True,
-    version='0.1.35_test_commit',
+    version='0.1.35',
     description='',
     author='Leonid Nidekker',
     author_email='leonidnidekker@gmail.com',
