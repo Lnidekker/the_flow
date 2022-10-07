5ac20169c426adf02a71a40f89b926bfd9d76007
Lnidekker <leonidnidekker@gmail.com>
2022-10-08 00:45:20 +0400

    v0.3.6

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='the_flow',
     packages=['the_flow', 'the_flow.templates'],
     include_package_data=True,
-    version='0.3.2',
-    description='remove "Please, use [-update_run_dir [all] [cfg] [step_scripts] [input_data]] after [-ui_mode terminal]." check',
+    version='0.3.6',
+    description='change error messages in the_flow bash script',
     author='Leonid Nidekker',
     author_email='leonidnidekker@gmail.com',
     license='MIT'
