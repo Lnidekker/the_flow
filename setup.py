3a3735fb1f5a3eb8139c8cfb14857796e93b63fb
Lnidekker <leonidnidekker@gmail.com>
2022-10-09 22:23:08 +0400

    add str() for sys.argv[*]

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='the_flow',
     packages=['the_flow', 'the_flow.templates'],
     include_package_data=True,
-    version='0.4.0',
-    description='start of release history',
+    version='0.4.1',
+    description='add str() for sys.argv[*]',
     author='Leonid Nidekker',
     author_email='leonidnidekker@gmail.com',
     license='MIT'
--- the_flow/the_flow_scripts.py
+++ the_flow/the_flow_scripts.py
@@ -15,11 +15,11 @@ import q2
 if __name__ == "__main__":
 
     # Set arguments value of python3 command to global_tf_vars
-    global_tf_vars.tf_cfg_dir = sys.argv[1]
+    global_tf_vars.tf_cfg_dir = str(sys.argv[1])
     global_tf_vars.tf_is_syn = int(sys.argv[2])
     global_tf_vars.tf_is_impl = int(sys.argv[3])
     global_tf_vars.tf_is_atpg = int(sys.argv[4])
-    global_tf_vars.tf_ux_ui_mode = sys.argv[5]
+    global_tf_vars.tf_ux_ui_mode = str(sys.argv[5])
     global_tf_vars.tf_update_run_dir = int(sys.argv[6])
     global_tf_vars.tf_update_all = int(sys.argv[7])
     global_tf_vars.tf_update_cfg = int(sys.argv[8])
