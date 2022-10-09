56077ff96c089c4b25be9fb7fb9b06c530275b57
Lnidekker <leonidnidekker@gmail.com>
2022-10-09 21:51:43 +0400

    start of release history

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='the_flow',
     packages=['the_flow', 'the_flow.templates'],
     include_package_data=True,
-    version='0.3.6',
-    description='change error messages in the_flow bash script',
+    version='0.4.0',
+    description='start of release history',
     author='Leonid Nidekker',
     author_email='leonidnidekker@gmail.com',
     license='MIT'
