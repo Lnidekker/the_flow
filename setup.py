de7593fb54c9c0d3466d2ab6d0754c42207ff17e
Lnidekker <111684495+Lnidekker@users.noreply.github.com>
2022-10-31 10:12:08 +0400

    v0.5.0 - main release (#4)

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='the_flow',
     packages=['the_flow', 'the_flow.templates'],
     include_package_data=True,
-    version='0.4.26',
-    description='add cl to phy_gen; tested',
+    version='0.5.0',
+    description='main release',
     author='Leonid Nidekker',
     author_email='leonidnidekker@gmail.com',
     license='MIT'
