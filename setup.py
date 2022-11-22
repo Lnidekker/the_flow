3810dd254217f39dacd3d70ca2c444f69a680973
Lnidekker <leonidnidekker@gmail.com>
2022-11-22 10:04:49 +0400

    v0.6.12 - add tf_complete function ; tested

--- setup.py
+++ setup.py
@@ -4,9 +4,8 @@ setup(
     name='the_flow',
     packages=['the_flow', 'the_flow.templates'],
     include_package_data=True,
-    version='0.6.6',
-    description='fix error when step into table have '' name ; make tcl scripts creator more stable but from now '
-                'step name must start from tf_*',
+    version='0.6.12',
+    description='add tf_complete function ; tested',
     author='Leonid Nidekker',
     author_email='leonidnidekker@gmail.com',
     license='MIT'
