6e58923e77bc923195113498d689a98de8a1a35d
Lnidekker <leonidnidekker@gmail.com>
2022-11-23 11:11:58 +0400

    v0.7.0 - main release

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='the_flow',
     packages=['the_flow', 'the_flow.templates'],
     include_package_data=True,
-    version='0.6.15',
-    description='mmmc_gen for partitions ; tested',
+    version='0.7.0',
+    description='main release',
     author='Leonid Nidekker',
     author_email='leonidnidekker@gmail.com',
     license='MIT'
