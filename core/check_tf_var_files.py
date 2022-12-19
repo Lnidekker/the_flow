4dfe272111f53992f30d62cdd0948208bbea8b60
Lnidekker <leonidnidekker@gmail.com>
2022-12-19 22:57:05 +0400

    0.7.28 - change tf_var.py searching to fix bug

--- core/check_tf_var_files.py
+++ core/check_tf_var_files.py
@@ -1,17 +1,19 @@
 import os
 import global_tf_vars
 from messages import messages
+import common_func
 
 
 def tf_var_finding(path):
     for element in os.scandir(path):
-        if element.is_file() and element.name == 'tf_var.py':
-            if global_tf_vars.tf_var_files == '':
-                global_tf_vars.tf_var_files = element.path
-            else:
-                global_tf_vars.tf_var_files = global_tf_vars.tf_var_files + ' ' + element.path
-        elif element.is_dir():
+        if element.is_dir():
             tf_var_finding(element.path)
+        elif common_func.tf_file_exists_check(element.path) == 'True':
+            if element.is_file() and element.name == 'tf_var.py':
+                if global_tf_vars.tf_var_files == '':
+                    global_tf_vars.tf_var_files = element.path
+                else:
+                    global_tf_vars.tf_var_files = global_tf_vars.tf_var_files + ' ' + element.path
 
 
 def run_check_tf_var_files():
